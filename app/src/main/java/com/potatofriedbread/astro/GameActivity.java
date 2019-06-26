638e3c44f5c106cb3e84ce8407b194326619b2d2
mmt <friedsoda@gmail.com>
2019-06-26 21:12:50 +0800

    //Dice

--- app/src/main/java/com/potatofriedbread/astro/GameActivity.java
+++ app/src/main/java/com/potatofriedbread/astro/GameActivity.java
@@ -3,8 +3,6 @@ package com.potatofriedbread.astro;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
